4fe834765d9e1f860b53999b660b19b7be079f75
amdazm05 <amdali1998@yahoo.com>
2023-03-19 01:15:35 +0500

    feat: thread safe version of HTTP client configurations updated w.ref to Anable motion sensor

--- main/includes/httpclient.hpp
+++ main/includes/httpclient.hpp
@@ -2,19 +2,23 @@
 #define _HTTPCLIENT
 
 #include "env/env.hpp"
-#include "env/cert/pem_cert.hpp"
 #include "utils/util.hpp"
 
 class HTTPApiClient
 {
     public:
         HTTPApiClient();
-        static void HTTPClient_init();
-        static void PostPacketToApi(std::string json, const char *  URL);
-        static void GetHttpRequestsTask(void);
+        static std::string GetPacketFromAPI(const char *);
+        static void PostPacketToApi(std::string && json, const char *  URL);
+        static esp_err_t _http_event_handle(esp_http_client_event_t *evt);
+        static esp_err_t _http_event_handle_write(esp_http_client_event_t *evt);
     private:
-        static esp_http_client_config_t httpClientConfig;
+        static esp_http_client_config_t httpClientConfigGet;
+        static esp_http_client_config_t httpClientConfigPost;
+        static esp_http_client_handle_t clientWrite;
         static esp_http_client_handle_t client;
+        static char buffer[1<<10];
+        static int recievedBytes;
 };
 
 #endif //_HTTPCLIENT
--- main/src/httpclient.cc
+++ main/src/httpclient.cc
@@ -1,9 +1,16 @@
 #include "httpclient.hpp"
 
-esp_http_client_config_t HTTPApiClient::httpClientConfig = {0};
-esp_http_client_handle_t HTTPApiClient::client={0};
+esp_http_client_config_t HTTPApiClient::httpClientConfigGet = {0};
+esp_http_client_config_t HTTPApiClient::httpClientConfigPost = {0};
 
-esp_err_t _http_event_handle(esp_http_client_event_t *evt)
+
+esp_http_client_handle_t HTTPApiClient::clientWrite={0};
+esp_http_client_handle_t HTTPApiClient::client={0};
+int HTTPApiClient::recievedBytes=0;
+
+char HTTPApiClient::buffer[1<<10];
+
+esp_err_t HTTPApiClient::_http_event_handle_write(esp_http_client_event_t *evt)
 {
     switch(evt->event_id) {
         case HTTP_EVENT_ERROR:
@@ -20,10 +27,51 @@ esp_err_t _http_event_handle(esp_http_client_event_t *evt)
             printf("\n %.*s \n", evt->data_len, (char*)evt->data);
             break;
         case HTTP_EVENT_ON_DATA:
-            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-            if (!esp_http_client_is_chunked_response(evt->client)) {
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_DATA, len=%d \n ", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) 
+            {
                 printf("\n %.*s \n", evt->data_len, (char*)evt->data);
             }
+            recievedBytes+=evt->data_len;
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_DISCONNECTED");
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_REDIRECT");
+            break;
+    }
+    return ESP_OK;
+}
+
+
+esp_err_t HTTPApiClient::_http_event_handle(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_HEADER");
+            printf("\n %.*s \n", evt->data_len, (char*)evt->data);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_DATA, len=%d \n ", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) 
+            {
+                printf("\n %.*s \n", evt->data_len, (char*)evt->data);
+                std::copy((char*)evt->data, (char*)evt->data+ evt->data_len , buffer+recievedBytes);
+            }
+            recievedBytes+=evt->data_len;
             break;
         case HTTP_EVENT_ON_FINISH:
             ESP_LOGI("HTTPClient", "HTTP_EVENT_ON_FINISH");
@@ -42,51 +90,46 @@ HTTPApiClient::HTTPApiClient()
 {
 }
 
-void HTTPApiClient::HTTPClient_init()
+std::string HTTPApiClient::GetPacketFromAPI(const char *  URL)
 {
-    httpClientConfig  =  {0};
-    httpClientConfig.url = API_URI;
-    httpClientConfig.cert_pem = PEM_CERT;
-    httpClientConfig.event_handler = _http_event_handle; 
+    memset(buffer,0,sizeof(buffer));
+    recievedBytes =0;
+    httpClientConfigGet  =  {0};
+    httpClientConfigGet.url = URL;
+    httpClientConfigGet.cert_pem = PEM_CERT;
+    httpClientConfigGet.event_handler = HTTPApiClient::_http_event_handle; 
 
-    client = esp_http_client_init(&httpClientConfig);   
+    client = esp_http_client_init(&httpClientConfigGet);   
     esp_err_t err = esp_http_client_perform(client);
     if (err == ESP_OK)
     {
-        std::cout<< "Status , content_length" <<
-                 esp_http_client_get_status_code(client)<<"  "<<
-                 esp_http_client_get_content_length(client)<<std::endl;
+        std::cout<< "Read content_length :" <<esp_http_client_get_content_length(client)<<std::endl;
     }
     esp_http_client_cleanup(client);
+    return std::move(std::string(buffer));
+
 }
 
-void HTTPApiClient::PostPacketToApi(std::string json, const char *  URL)
+void HTTPApiClient::PostPacketToApi(std::string && json, const char *  URL)
 {
-    // std::cout<<json <<"     "<<URL;
-    httpClientConfig = {0};
-    httpClientConfig.url = URL;
-    httpClientConfig.event_handler = _http_event_handle;
-    httpClientConfig.cert_pem = PEM_CERT;
-    httpClientConfig.method = HTTP_METHOD_POST;
+    ESP_LOGW("WARN","%s",URL );
+    httpClientConfigPost = {0};
+    httpClientConfigPost.url = URL;
+    httpClientConfigPost.event_handler = _http_event_handle_write;
+    httpClientConfigPost.cert_pem = PEM_CERT;
+    httpClientConfigPost.method = HTTP_METHOD_POST;
 
-    client = esp_http_client_init(&httpClientConfig);
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-    esp_http_client_set_post_field(client, json.c_str(), json.size());
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-    // httpClientConfig = {0};
-    // httpClientConfig.url = URL;
-    // httpClientConfig.event_handler = _http_event_handle;
-    // esp_http_client_set_post_field(client, json.data(),json.size());
-    esp_err_t err = esp_http_client_perform(client);
+    clientWrite = esp_http_client_init(&httpClientConfigPost);
+    esp_http_client_set_method(clientWrite, HTTP_METHOD_POST);
+    esp_http_client_set_post_field(clientWrite, json.c_str(), json.size());
+    esp_http_client_set_header(clientWrite, "Content-Type", "application/json");
+
+    esp_err_t err = esp_http_client_perform(clientWrite);
     if (err == ESP_OK) {
-        std::cout<<"##### HTTP POST Status = "<<esp_http_client_get_status_code(client)<<std::endl;
+        std::cout<<"HTTP POST Status : "<<esp_http_client_get_status_code(clientWrite)<<std::endl;
     } else {
-        ESP_LOGW("TAG", "HTTP POST request failed: %s", esp_err_to_name(err));
+        ESP_LOGW("HTTPClient", "HTTP POST request failed: %s", esp_err_to_name(err));
     }
 
-    esp_http_client_cleanup(client);
-}
-void HTTPApiClient::GetHttpRequestsTask(void)
-{
-
+    esp_http_client_cleanup(clientWrite);
 }
